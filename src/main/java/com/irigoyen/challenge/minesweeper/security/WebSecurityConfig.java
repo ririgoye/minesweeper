2c116865cff3790a3d4c2088771c109f4e2d5202
rodiri <rodrigo@6ripples.com>
2021-03-07 19:05:32 -0600

    Added CORS validation

---
+++ src/main/java/com/irigoyen/challenge/minesweeper/security/WebSecurityConfig.java
@@ -0,0 +1,36 @@
+package com.irigoyen.challenge.minesweeper.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter
+{
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception
+    {
+        http.csrf().disable().authorizeRequests().anyRequest().permitAll();
+    }
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer()
+    {
+        return new WebMvcConfigurer()
+        {
+            @Override
+            public void addCorsMappings(CorsRegistry registry)
+            {
+                registry
+                        .addMapping("/**")
+                        .allowedOrigins("http://localhost:3000","https://rip-minesweeper-ui.herokuapp.com")
+                        .allowedMethods("OPTIONS", "GET", "PUT", "POST", "DELETE");
+
+            }
+        };
+    }
+}
